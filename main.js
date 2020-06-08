72d5836746e1b373690b6874cbe3deda8dba6181
Satyaki-Roy <satyakiroy001@gmail.com>
2020-06-08 20:58:00 +0530

    including ffmpeg binary into the project

--- main.js
+++ main.js
@@ -7,6 +7,23 @@ const imageminPngquant = require("imagemin-pngquant");
 const slash = require("slash");
 const log = require("electron-log");
 
+
+//require the ffmpeg package so we can use ffmpeg using JS
+const ffmpeg = require("fluent-ffmpeg");
+//Get the paths to the packaged versions of the binaries we want to use
+const ffmpegPath = require("ffmpeg-static").replace(
+  "app.asar",
+  "app.asar.unpacked"
+);
+const ffprobePath = require("ffprobe-static").path.replace(
+  "app.asar",
+  "app.asar.unpacked"
+);
+//tell the ffmpeg package where it can find the needed binaries.
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+
+
 // Set env
 process.env.NODE_ENV = "development";
 
